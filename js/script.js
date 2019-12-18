0f7ea059061015f7c1c92b9145797d94bdfa9097
Federico <federico.caiello@gmail.com>
2019-12-18 15:40:56 +0100

    train ticket

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// numero di chilometri da percorrere
+var chilometriUtente = prompt("numero di chilometri da percorrere?");
+console.log(chilometriUtente);
+var etaPasseggero = prompt("quale' la tua eta'?");
+console.log(etaPasseggero);
+// prezzo totale del biglietto
