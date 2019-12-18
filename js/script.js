90051d7c8234ffd1c021d17a0d9d6b2db9399ff7
Federico <federico.caiello@gmail.com>
2019-12-18 17:25:31 +0100

    discount age

--- js/script.js
+++ js/script.js
@@ -4,3 +4,13 @@ console.log(chilometriUtente);
 var etaPasseggero = prompt("quale' la tua eta'?");
 console.log(etaPasseggero);
 // prezzo totale del biglietto
+var costoBiglietto = chilometriUtente * 0.21; // moltiplicazione, con il segno*
+console.log(costoBiglietto);
+// condizioni sconto Biglietto
+if (etaPasseggero < 18){
+  // minorenne sconto del 20%
+console.log(costoBiglietto - (costoBiglietto * 20) / 100);
+} else if (etaPasseggero > 65) {
+  // over 65 anni di eta' sconto 40%
+  console.log(costoBiglietto - (costoBiglietto * 40) / 100);
+}
